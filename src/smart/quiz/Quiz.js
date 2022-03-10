36e468f02789dfa601927ccd2cd5c0e55f3eb00a
Dulaya <dulaya.saennok@gmail.com>
2022-03-09 17:55:47 -0800

    first commit

---
+++ src/smart/quiz/Quiz.js
@@ -0,0 +1,23 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {QuizView} from './Quiz.view';
+
+export function Quiz() {
+  // mock
+  const mockData = {
+    question: 'What const is doing?',
+    answers: [
+      'Create variable',
+      'define function',
+      'Shot down computer',
+      'And one more stupid idea',
+    ],
+    correctAnswer: 3,
+  };
+
+  const checkAnswer = index => {
+    console.log("I'm checking the answer", index);
+  };
+
+  return <QuizView data={mockData} press={checkAnswer} />;
+}
